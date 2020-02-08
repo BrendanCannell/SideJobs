5b63d623f8c2115d214299880b0ca44c42d5d476
laportenier <lilly.portenier@gmail.com>
2020-02-08 11:24:46 -0500

    started pull request in results file

--- sidejobsreact/client/src/components/Card/index.js
+++ sidejobsreact/client/src/components/Card/index.js
@@ -1,36 +1,42 @@
 import React from "react";
 import { Card, Heading, Text } from "rebass";
-// import Results from "../../pages/Results/Results";
+import Results from "../../pages/Results/Results";
 // import { PromiseProvider } from "mongoose";
 
 
 
+// export function CardData({ children }) {
+//     return <li>{children}</li>;
+// }
 
+// export function Card({ children }) {
+//     return (
+//         <div className="list-overflow-container">
+//             <ul className="list-group">{children}</ul>
+//         </div>
+//     );
+// }
+// export function ResultCard(props) {
+//     return (
+//         <div>
+//             <Card key={item.id}>
+//                 <div className='ResultCardStyle'>
+//                     <div className='ResultsHeaderInfoFloat'>
+//                         <div className='CardHearderInfoFloat'>
+//                             <Heading className='CardTitleStyle'>{item.service}</Heading>
+//                             <div className='ZipAndPriceStyle'>
+//                                 <Text className='CardPriceStyle'>{item.costPerHour}</Text>
+//                                 <Text className='CardZipcodeStyle'>{item.city} {item.zipCode} </Text>
+//                             </div>
+//                         </div>
+//                     </div>
+//                     <Text className='CardTextStyle'>{item.description}</Text>
+//                 </div>
+//             </Card>
+//         </div>
+//     );
+// }
 
-export function ResultCard(props) {
-    return (
-        <div>
-            {props.jobScema.map(item => (
-                console.log(item)
-                // <Card key={item.id}>
-                //     <div className='ResultCardStyle'>
-                //         <div className='ResultsHeaderInfoFloat'>
-                //             <div className='CardHearderInfoFloat'>
-                //                 <Heading className='CardTitleStyle'>{item.service}</Heading>
-                //                 <div className='ZipAndPriceStyle'>
-                //                     <Text className='CardPriceStyle'>{item.costPerHour}</Text>
-                //                     <Text className='CardZipcodeStyle'>{item.city} {item.zipCode} </Text>
-                //                 </div>
-                //             </div>
-                //         </div>
-                //         <Text className='CardTextStyle'>{item.description}</Text>
-                //     </div>
-                // </Card>
-            ))}
-        </div>
-    );
-}
-
-export default ResultCard;
+// export default ResultCard;
 
 // city and zip code together, email , title= service 
--- sidejobsreact/client/src/pages/Results/Results.js
+++ sidejobsreact/client/src/pages/Results/Results.js
@@ -1,10 +1,62 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from 'axios';
+import API from '../../utils'
 // import { Card, Image, Heading, Text } from "rebass";
 import './style.css';
-import ResultCard from '../../components/Card';
-// import { ThemeProvider } from 'emotion-theming'
+
+// import { CardData, Card } from '../../components/Card';
+import { Card, Heading, Text, Image } from "rebass";
 
 
+class Results extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            Jobs: []
+        };
+    };
+
+    componentDidMount() {
+        this.loadJobs();
+    };
+    loadJobs(){
+
+        API.getJobs()
+            // line above needs proper url for search page, parocees.env? /jobs? port number?
+            .then(res => {
+            this.setState({ Jobs: res.data });
+            // console.log(Jobs)
+        }).catch(function (err) {
+            console.log(err);
+        })
+    }
+    jobResults() {
+        return this.state.Jobs.map((data, i) => {
+            return (
+                <div>
+                    <Card>
+                        <div className='ResultCardStyle'>
+                            <div className='ResultsHeaderInfoFloat'>
+                                <div>
+                                    <Image className='CardImageStyle' src={'https://via.placeholder.com/150x150'} />
+                                </div>
+                                <div className='CardHearderInfoFloat'>
+                                    <Heading className='CardTitleStyle'>Title Title Title Title Title Title </Heading>
+                                    <div className='ZipAndPriceStyle'>
+                                        <Text className='CardPriceStyle'> Pricing/hr</Text>
+                                        <Text className='CardZipcodeStyle'> ZipCode </Text>
+                                    </div>
+                                </div>
+                            </div>
+                            <Text className='CardTextStyle'> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
+                        </div>
+                    </Card>
+                </div>
+
+            )
+        })
+    }
+}
 
 // const ResultCardStyle = {
 //     border: '30px, solid, black',
@@ -16,34 +68,6 @@ import ResultCard from '../../components/Card';
 
 // };
 
-function Results(props) {
-    return (
-        <div>
-            <ResultCard />
-        </div>
-        // <div>
-        //     <Card>
 
-        //         <div className='ResultCardStyle'>
-        //             <div className='ResultsHeaderInfoFloat'>
-        //                 <div>
-        //                     <Image className='CardImageStyle' src={'https://via.placeholder.com/150x150'} />
-        //                 </div>
-        //                 <div className='CardHearderInfoFloat'>
-        //                     <Heading className='CardTitleStyle'>Title Title Title Title Title Title </Heading>
-        //                     <div className='ZipAndPriceStyle'>
-        //                         <Text className='CardPriceStyle'> Pricing/hr</Text>
-        //                         <Text className='CardZipcodeStyle'> ZipCode </Text>
-        //                     </div>
-        //                 </div>
-        //             </div>
-        //             <Text className='CardTextStyle'> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
-        //         </div>
-        //     </Card>
-        // </div>
-
-
-    )
-}
 
 export default Results;
