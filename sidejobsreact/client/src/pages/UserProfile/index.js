cb41d7dea8dea4e954230c80fda0e9b05e387f86
laportenier <lilly.portenier@gmail.com>
2020-02-11 20:35:37 -0500

    got get request for user profile by id working

--- sidejobsreact/client/src/pages/UserProfile/index.js
+++ sidejobsreact/client/src/pages/UserProfile/index.js
@@ -7,14 +7,16 @@ class UserProfile extends Component{
   state = {
     users: []
   };
-  // componentDidMount() {
-  //   console.log("inot api")
-  //     API.getUserProfile(this.props.match.params.id)
-  //     .then(res => {
-  //       this.setState ({ users: res.data });
-  //       console.log(res, "res data");
-  //     }).catch (err => console.log(err));
-  // };
+  
+  componentDidMount() {
+    console.log("inot api")
+      API.getUserProfile(this.props.match.params.id)
+      .then(res => {
+        this.setState ({ users: res.data });
+        console.log(res, "res data");
+      }).catch (err => console.log(err));
+  };
+
   render() {
     return (
       <div> you made it </div>
