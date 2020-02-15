dcec2134a6d66ca53034f9803f2e016a8cfdfda8
Gabriel Mixon <gabrielmixon@gmail.com>
2020-02-15 13:08:39 -0500

    got users showing up in the db

--- sidejobsreact/controllers/AuthControllers.js
+++ sidejobsreact/controllers/AuthControllers.js
@@ -1,29 +1,28 @@
-var mongoose = require("mongoose");
+// var mongoose = require("mongoose");
 var passport = require("passport");
 var User = require("../models/User");
-var Job = require('../models/Job')
-var userController = require('./userController')
-
+// var Job = require('../models/Job')
+// var userController = require('./userController')
+const db = require("../models");
 
 // Restrict access to root page
 module.exports = {
   home: function (req, res) {
-    User
+  db.User
       .then(res.redirect('/', { user: req.user }));
   },
 
   // Go to registration page
   register: function (req, res) {
-    User
+    db.User
       .then(res.render('signup'));
   },
 
   // Post registration
   doRegister: function (req, res) {
     console.log(req.body)
-    User
-      .create(
-        {
+    db.User
+      .create({
           firstName: req.body.firstName,
           lastName: req.body.lastName,
           phoneNumber: req.body.phoneNumber,
@@ -31,31 +30,26 @@ module.exports = {
           password: req.body.password,
           zip: req.body.zip,
           city: req.body.city
-        },
-        // req.body.password,
-        function (err, user) {
-          // if (err) {
-          //   return res.redirect('signup', { user: user });
-          // }
-
+        })
+        .then(user => {
+          console.log(user)
           passport.authenticate('local')(req, res, function () {
-            // res.redirect('/');
+            res.json(user);
           });
-        }
-      );
+        }) 
   },
 
   // Go to login page
   
   login: function (req, res) {
-    User
+    db.User
       res.redirect('signin');
   },
 
 
 // Post login
 doLogin: function (req, res) {
-  User
+  db.User
     passport.authenticate('local')(req, res, function () {
     res.redirect('/');
   });
@@ -63,7 +57,7 @@ doLogin: function (req, res) {
 
   // logout
   logout: function (req, res) {
-    User
+    db.User
       req.logout();
       res.redirect('/');
   }, 
